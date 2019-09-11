011afe65639750ea358bda6fadbe057d6097c937
Nathan <nystera@gmail.com>
2019-09-11 22:27:21 +0800

    Added minimal testing.

---
+++ src/test/java/ToDoTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+
+    private final String expected = "[T][" + "\u2718" + "] test description";
+
+    @Test
+    public void test() {
+        ToDo todo = new ToDo("test description");
+        String outputString = todo.toString();
+        assertEquals(expected, outputString);
+    }
+
+}
